76912424d9d7b4bed544c8fd0d74e23f04caa698
Andremartiny <andremartiny91@gmail.com>
2024-08-24 18:33:20 +0200

    Quartz sync: Aug 24, 2024, 6:33 PM

--- quartz/components/pages/FolderContent.tsx
+++ quartz/components/pages/FolderContent.tsx
@@ -3,25 +3,21 @@ import path from "path"
 
 import style from "../styles/listPage.scss"
 import { PageList, SortFn } from "../PageList"
-import { FolderList } from "../FolderList"
-import { stripSlashes, simplifySlug, SimpleSlug, joinSegments } from "../../util/path"
+import { stripSlashes, simplifySlug } from "../../util/path"
 import { Root } from "hast"
 import { htmlToJsx } from "../../util/jsx"
 import { i18n } from "../../i18n"
-import { QuartzPluginData } from "../../plugins/vfile"
 
 interface FolderContentOptions {
   /**
    * Whether to display number of folders
    */
   showFolderCount: boolean
-  showSubfolders: boolean
   sort?: SortFn
 }
 
 const defaultOptions: FolderContentOptions = {
   showFolderCount: true,
-  showSubfolders: true,
 }
 
 export default ((opts?: Partial<FolderContentOptions>) => {
@@ -30,39 +26,20 @@ export default ((opts?: Partial<FolderContentOptions>) => {
   const FolderContent: QuartzComponent = (props: QuartzComponentProps) => {
     const { tree, fileData, allFiles, cfg } = props
     const folderSlug = stripSlashes(simplifySlug(fileData.slug!))
-
-    const allPagesInFolder: QuartzPluginData[] = []
-    const allSubfolders: Set<SimpleSlug> = new Set()
-
-    allFiles.forEach((file) => {
+    const allPagesInFolder = allFiles.filter((file) => {
       const fileSlug = stripSlashes(simplifySlug(file.slug!))
       const prefixed = fileSlug.startsWith(folderSlug) && fileSlug !== folderSlug
       const folderParts = folderSlug.split(path.posix.sep)
       const fileParts = fileSlug.split(path.posix.sep)
       const isDirectChild = fileParts.length === folderParts.length + 1
-
-      if (!prefixed) {
-        return
-      }
-
-      if (isDirectChild) {
-        allPagesInFolder.push(file)
-      } else {
-        const folderSlug = joinSegments(...fileParts.slice(0, folderParts.length + 1))
-        allSubfolders.add(folderSlug as SimpleSlug)
-      }
+      return prefixed && isDirectChild
     })
-
     const cssClasses: string[] = fileData.frontmatter?.cssclasses ?? []
     const classes = ["popover-hint", ...cssClasses].join(" ")
-    const pageListProps = {
+    const listProps = {
       ...props,
       sort: options.sort,
       allFiles: allPagesInFolder,
-    }
-    const folderListProps = {
-      ...props,
-      allFolders: Array.from(allSubfolders.values()),
     }
 
     const content =
@@ -82,15 +59,7 @@ export default ((opts?: Partial<FolderContentOptions>) => {
             </p>
           )}
           <div>
-            <PageList {...pageListProps} />
-          </div>
-        </div>
-        <div class="page-listing">
-          {options.showSubfolders && (
-            <p>{allSubfolders.size === 1 ? "1 subfolder." : `${allSubfolders.size} subfolders.`}</p>
-          )}
-          <div>
-            <FolderList {...folderListProps} />
+            <PageList {...listProps} />
           </div>
         </div>
       </div>
