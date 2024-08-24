d701bd1266d153208b0386dac237365e440bbe1f
Andremartiny <andremartiny91@gmail.com>
2024-08-24 21:55:24 +0200

    Quartz sync: Aug 24, 2024, 9:55 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -14,7 +14,7 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
+    // Component.Breadcrumbs(),
     Component.ArticleTitle(),
     // Component.ContentMeta(),
     Component.TagList(),
@@ -32,7 +32,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle()], // , Component.ContentMeta()
+  beforeBody: [Component.ArticleTitle()], // , Component.ContentMeta()Component.Breadcrumbs(), 
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
